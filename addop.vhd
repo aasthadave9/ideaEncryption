0d18856b5b52c18bbbe1c7c2861e8ce349b31558
Aastha Dave <86701156+aasthadave9@users.noreply.github.com>
2023-05-13 15:21:53 +0200

    Create addop.vhd

---
+++ addop.vhd
@@ -0,0 +1,46 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: Aastha Dave
+-- 
+-- Create Date:    15:07:02 05/08/2023 
+-- Design Name:    Module 2^16 Adder
+-- Module Name:    addop - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity addop is
+    Port ( a : in  STD_LOGIC_VECTOR (15 downto 0);
+           b : in  STD_LOGIC_VECTOR (15 downto 0);
+           sum : out  STD_LOGIC_VECTOR (15 downto 0));
+end addop;
+
+architecture Behavioral of addop is
+begin
+	
+	sum <= (a + b);
+
+end Behavioral;
+
